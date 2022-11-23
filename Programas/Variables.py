0220e6e94f14f49ad9ef7c1147eb111ba9d4e38e
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-11-23 11:23:41 +0000

    Cambio 1.1

---
+++ Programas/Variables.py
@@ -0,0 +1,6 @@
+'Creando variables en python'
+'Según la convención de Python las variables se crearán en minúsculas, separando las letras con un _'
+
+my_variable = 'Hola mundo'
+
+print(my_variable)
