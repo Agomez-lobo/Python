67f447ab3d0235ee8c6ddeeca7fe9a541cfee063
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-11-26 21:59:22 +0100

    1.5

---
+++ Programas/02_operadores.py
@@ -0,0 +1,24 @@
+#OPERADORES
+
+"""
++	Realiza Adición entre los operandos	                12 + 3 = 15
+-	Realiza Substracción entre los operandos	        12 - 3 = 9
+*	Realiza Multiplicación entre los operandos	        12 * 3 = 36
+/	Realiza División entre los operandos	            12 / 3 = 4
+%	Realiza un módulo entre los operandos	            16 % 3 = 1
+**	Realiza la potencia de los operandos	            12 ** 3 = 1728
+//	Realiza la división con resultado de número entero	18 // 5 = 3
+"""
+
+print(12 + 3)
+print(12 - 3)
+print(12 * 3)
+print(12 / 3)
+print(12 % 3)
+print(12 ** 3)
+print(12 // 3)
+
+#Algunos símbolos como el "+" nos ayuda a concatenar por ejemplo cadenas, pero no podremos concatenar variables de diferente tipo
+#Para concatenar un string y un número por ejemplo.... habría que utilizar una función del sistema como str:
+
+print("Hola a todos " + "y a todas.")
