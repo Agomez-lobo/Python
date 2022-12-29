7d2ecfa1c5043b5402dbd6b93fc4e030e7563484
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-29 09:46:49 +0100

    2.0

--- Programas/07_diccionarios.py
+++ Programas/07_diccionarios.py
@@ -1,1 +1,8 @@
 #DICTS o DICCIONARIOS
+
+my_dict = dict()
+my_other_dict = {}
+
+print(type(my_dict))
+
+my_other_dict = {"Nombre":"Alejandro", "Apellido":"Gomez-Lobo", "Edad":38, "Lenguajes": {"Python","C#","Visual Basic"}}
