f6f16e221e35219f93d48c408f702a360d9296e7
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-30 11:31:33 +0000

    3.1

--- Programas/10_funciones.py
+++ Programas/10_funciones.py
@@ -1,2 +1,12 @@
 ## FUNCIONES
 
+def my_function():
+    print("Esto es una función")
+
+def suma(numero1, numero2):
+    valor = numero1 + numero2
+    return valor
+
+my_function()
+print(suma(15, 35))
+
