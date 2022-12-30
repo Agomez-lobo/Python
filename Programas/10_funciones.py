02c16a3e3af8c81fda44d4e8e3ecb84609b9fad3
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-30 11:40:16 +0000

    3.3

--- Programas/10_funciones.py
+++ Programas/10_funciones.py
@@ -7,6 +7,11 @@ def suma(numero1, numero2):
     valor = numero1 + numero2
     return valor
 
+def defecto(numero1, numero2 = 5):
+    print(numero1 + numero2)
+
 my_function()
 print(suma(15, 35))
+defecto(10)
+defecto(35,50)
 
