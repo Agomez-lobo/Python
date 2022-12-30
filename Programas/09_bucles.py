85298eb5b7845188818cbee3c58910db822930fd
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-30 10:41:51 +0000

    2.9

--- Programas/09_bucles.py
+++ Programas/09_bucles.py
@@ -7,8 +7,20 @@ my_condition = 0
 while my_condition < 20:
     print(my_condition)
     my_condition += 1
+    if my_condition == 15:
+        break
 
 if my_condition == 10:
     print("Hemos llegado a 10")
 else:
     print("Superamos el valor de 10")
+
+print("El valor conseguido es: %d" %(my_condition))
+
+#FOR
+
+my_list = [35, 24, 13, 11, 20, 5, 19,75]
+
+for elemento in my_list:
+    print(elemento)
+    
