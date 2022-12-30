95b9db12cbfc6bc5d734f7643269c6082a4a40fc
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-30 10:16:46 +0000

    2.7

--- Programas/08_condicionales.py
+++ Programas/08_condicionales.py
@@ -6,3 +6,12 @@ if my_valor:
     print("Verdadero")
 else:
     print("Falso")
+
+my_other_valor = 37
+
+if my_other_valor < 18:
+    print("Eres menor de edad")
+elif my_other_valor > 18 and my_other_valor < 40:
+    print("Eres un papi")
+else:
+    print("Jubilado")    
