fd023234ca100c6334e11a2daed282cb759d1fe6
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2022-12-30 10:36:52 +0000

    2.8

---
+++ Programas/09_bucles.py
@@ -0,0 +1,14 @@
+### Bucles
+
+#WHILE
+
+my_condition = 0
+
+while my_condition < 20:
+    print(my_condition)
+    my_condition += 1
+
+if my_condition == 10:
+    print("Hemos llegado a 10")
+else:
+    print("Superamos el valor de 10")
