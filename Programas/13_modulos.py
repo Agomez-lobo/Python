f754531f99cee83de65a3321ddccfc87a7889306
Alejandro <98462764+Agomez-lobo@users.noreply.github.com>
2023-01-24 12:47:34 +0000

    3.7

---
+++ Programas/13_modulos.py
@@ -0,0 +1,7 @@
+### MODULOS ##
+
+import my_module
+my_module.suma(15, 15)
+
+#from my_other_modulo import vis
+#vis("HOLA")
---
+++ Programas/my_module.py
@@ -0,0 +1,3 @@
+
+def suma(numero1, numero2):
+    print(numero1 + numero2)
